adb16331e7b36dba8f01b05afcaed1b53bd71678
abdelweheb <dhibabdelweheb@hotmail.com>
2023-02-16 22:41:17 -0800

    2

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,6 +6,7 @@ use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -14,11 +15,11 @@ class Article
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le Titre Est Obligatoire")]
     private ?string $titre = null;
-
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "IL Faut écrire qlq chose")]
     private ?string $contenu = null;
 
     #[ORM\OneToMany(mappedBy: 'article', targetEntity: Commentaire::class)]
--- src/Entity/Commentaire.php
+++ src/Entity/Commentaire.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CommentaireRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentaireRepository::class)]
@@ -14,6 +15,7 @@ class Commentaire
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "IL Faut écrire qlq chose")]
     private ?string $ContenuCommentaire = null;
 
     #[ORM\ManyToOne(inversedBy: 'Relation')]
